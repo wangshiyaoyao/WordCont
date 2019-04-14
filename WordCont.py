c01b0fd0b395bb724d06d421f701f0e011a42a57
wangshiyaoyao <675623495@qq.com>
2019-04-14 17:39:40 +0800

    commit code and test code

---
+++ WordCont.py
@@ -0,0 +1,125 @@
+# encoding=utf-8
+"""
+time: 2019-04-10
+IDE :pycharm + python3.7
+auther:
+功能：对文本文件中的单词的词频进行统计
+用法：Example：python WordCont.py **.txt
+"""
+# pylint: disable=invalid-name
+
+import sys
+import logging
+import re
+
+
+def get_argv():
+    '''获取第2个参数'''
+
+    argv = sys.argv
+    if len(argv) == 2:
+        return argv[1]
+    return ''
+
+
+def _word_check_in_line(line):
+    """单词匹配"""
+    pattern = r'(?<![a-zA-Z0-9])([a-zA-Z][0-9a-zA-Z]*)'
+    result = re.findall(pattern, line)
+    # logging.debug('word check in line result:%s', result)
+    return result
+
+
+def _file_check(filename):
+    """判断参数是一个可读文件，否则报错"""
+    try:
+        fd = open(filename)
+        fd.close()
+    except IOError:
+        logging.error("Invalid argument.\nNeed a readable file.")
+        sys.exit(1)
+
+
+class FileHandle:
+    """文件处理"""
+
+    def __init__(self, filename, encoding='utf-8'):
+        self._chars = 0                                               # 统计ascii
+        self._container = {}                                          # 统计词频
+        self._lines = 0                                               # 统计行数
+        self._words = 0                                               # 统计单词数
+        self._sorted_container = []                                   # 输出词频
+        _file_check(filename)
+        self._analysis(filename, encoding)
+
+    def _chars_analysis(self, line):
+        """统计ascii字符个数"""
+        self._chars += len(line)
+
+    def _line_analysis(self, line):
+        """统计有效行数"""
+        if line.strip():
+            self._lines += 1
+
+    def _word_analysis(self, line):
+        """统计词频"""
+        for word_match in _word_check_in_line(line):
+            word = word_match.lower()
+            self._container[word] = self._container.get(word, 0) + 1
+
+    def _word_sum(self, line):
+        """统计单词数"""
+        self._words += len(_word_check_in_line(line))
+
+    def _analysis(self, filename, encoding):
+        """文件分析统计"""
+        with open(filename, encoding=encoding) as fobj:
+            for line in fobj:
+                # logging.debug('read line:%s', line)
+                self._chars_analysis(line)
+                self._line_analysis(line)
+                self._word_analysis(line)
+                self._word_sum(line)
+        self._sort_container()              # 排序
+
+    def _sort_container(self):
+        """词频结果排序，获取前10结果"""
+        self._sorted_container = sorted(self._container.items(), key=lambda x: (-x[1], x[0]))[:10]
+
+    def chars(self):
+        """get chars"""
+        return self._chars
+
+    def container(self):
+        """get sorted container"""
+        return self._sorted_container
+
+    def lines(self):
+        """get lines"""
+        return self._lines
+
+    def words(self):
+        """get words"""
+        return self._words
+
+
+def out_result(chars, words, lines, container):
+    """结果输出"""
+    print('characters:', chars)
+    print('words:', words)
+    print('lines:', lines)
+    for word, number in container:
+        print(f'<{word}>:{number}')
+
+
+def main(filename):
+    """主函数"""
+
+    handle = FileHandle(filename)
+    result = (handle.chars(), handle.words(), handle.lines(), handle.container())
+    out_result(*result)
+
+
+if __name__ == '__main__':
+    main(get_argv())
+
---
+++ testWordCont.py
@@ -0,0 +1,69 @@
+"""Test for WordCont"""
+# pylint: disable=invalid-name
+
+import os
+import unittest
+
+from xeger import Xeger
+import WordCont
+
+temp_file = '.test.txt'
+
+
+def touch_test_file(line_num, word_num):
+    """创建测试文件，随机生成字符，用于测试"""
+
+    _x = Xeger()
+    words = lambda: _x.xeger(r'[a-zA-Z][a-zA-Z0-9]*')       # 随机生成有效单词
+    non_word = lambda: _x.xeger(r'\d[a-zA-Z0-9]*')          # 随机生成开头为数字的单词
+    separator = lambda: _x.xeger(r'[^a-zA-Z0-9\n\r]')       # 随机生成非字母数字回车换行符的字符
+    space = lambda: _x.xeger(r'\n[\s]*\n')                  # 随机生成回车空白字符回车
+
+    # 统计生成的文件中字符、单词、有效行、词频
+    result = {'chars': 0, 'words': word_num * line_num, 'lines': line_num, 'container': {}}
+
+    # 创建文件，随机生成字符
+    fd = open(temp_file, 'w')
+    for line in range(line_num):
+        for i in range(word_num):
+            word = words()
+            chars = word + separator() + non_word() + separator()
+            result['chars'] += len(chars)
+            result['container'][word.lower()] = result['container'].get(word.lower(), 0) + 1
+            fd.write(chars)
+        chars = space()
+        result['chars'] += len(chars)
+        fd.write(chars)
+    fd.close()
+
+    # 获取排序后的词频结果
+    sort_result = sorted(result['container'].items(), key=lambda x: (-x[1], x[0]))[:10]
+    result['container'] = sort_result
+    return result
+
+
+class TestWordCont(unittest.TestCase):
+    """Test for WordCont"""
+
+    def test_chars(self):
+        """字符统计测试"""
+        self.assertEqual(handle.chars(), result['chars'])
+
+    def test_words(self):
+        """单词测试"""
+        self.assertEqual(handle.words(), result['words'])
+
+    def test_lines(self):
+        """有效行测试"""
+        self.assertEqual(handle.lines(), result['lines'])
+
+    def test_container(self):
+        """词频测试"""
+        self.assertEqual(handle.container(), result['container'])
+
+
+result = touch_test_file(100, 40)
+handle = WordCont.FileHandle(temp_file)
+if __name__ == '__main__':
+    unittest.main()
+    os.remove(temp_file)
